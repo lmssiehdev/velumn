273bbf4d407f5cf641daf561565551977350f7ee
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-09-25 04:47:18 +0100

    fix(types): fix build

--- packages/db/src/wipe.ts
+++ packages/db/src/wipe.ts
@@ -1,28 +1,28 @@
-import pg from 'pg';
-import { url } from './index.js';
+// import pg from 'pg';
+// import { url } from './index.js';
 
-async function dropDatabase() {
-  if (process.env.NODE_ENV === 'production') {
-    return;
-  }
-  const pool = new pg.Pool({
-    connectionString: url,
-  });
+// async function dropDatabase() {
+//   if (process.env.NODE_ENV === 'production') {
+//     return;
+//   }
+//   const pool = new pg.Pool({
+//     connectionString: url,
+//   });
 
-  try {
-    await pool.query(
-      `SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'discord-indexer'`
-    );
-    await pool.query('DROP DATABASE IF EXISTS "discord-indexer"');
-    await pool.query(
-      'CREATE DATABASE "discord-indexer" OWNER "discord-indexer"'
-    );
-    console.info('Database wiped successfully');
-  } catch (error) {
-    console.error('Error dropping database:', error);
-  } finally {
-    await pool.end();
-  }
-}
+//   try {
+//     await pool.query(
+//       `SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'discord-indexer'`
+//     );
+//     await pool.query('DROP DATABASE IF EXISTS "discord-indexer"');
+//     await pool.query(
+//       'CREATE DATABASE "discord-indexer" OWNER "discord-indexer"'
+//     );
+//     console.info('Database wiped successfully');
+//   } catch (error) {
+//     console.error('Error dropping database:', error);
+//   } finally {
+//     await pool.end();
+//   }
+// }
 
-dropDatabase();
+// dropDatabase();
