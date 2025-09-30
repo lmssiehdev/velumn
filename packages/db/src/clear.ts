b9e6cad752b34223e9bf1cbcfc20e8f071681147
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-09-30 01:32:56 +0100

    fix: fix db clearing script

--- packages/db/src/clear.ts
+++ packages/db/src/clear.ts
@@ -1,25 +1,24 @@
-// import { db } from '.';
-// // biome-ignore lint/performance/noNamespaceImport: <explanation>
-// import * as schema from './schema';
+import { db } from '.';
+// biome-ignore lint/performance/noNamespaceImport: <explanation>
+import * as schema from './schema';
 
-// export async function clearAllTables() {
-//   const tables = [
-//     schema.dbAttachments,
-//     schema.dbMessage,
-//     schema.dbChannel,
-//     schema.dbServer,
-//     schema.dbDiscordUser,
-//     schema.user,
-//     schema.session,
-//     schema.account,
-//     schema.verification,
-//   ];
+export async function clearAllTables() {
+    const tables = [
+        schema.dbAttachments,
+        schema.dbMessage,
+        schema.dbChannel,
+        schema.dbServer,
+        schema.dbDiscordUser,
+        schema.user,
+        schema.session,
+        schema.account,
+        schema.verification,
+    ];
 
-//   for (const table of tables) {
-//     await db.delete(table);
-//   }
+    for (const table of tables) {
+        await db.delete(table);
+    }
 
-//   await db.$client.end();
-// }
+}
 
-// clearAllTables();
+await clearAllTables();
