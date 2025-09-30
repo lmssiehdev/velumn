0426508f77aba949b920fbc227f707b21835c46c
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-09-30 16:31:28 +0100

    fix missing database_url

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
-import { dbEnv } from './src/env';
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: 'src/schema/index.ts',
   out: './src/drizzle',
   dbCredentials: {
-    url: dbEnv.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -6,7 +6,7 @@ import { dbEnv } from './env';
 // TODO: move to env lol
 // export const DATABASE_URL =
 //   'postgresql://neondb_owner:npg_QKinmFA0UN1D@ep-tiny-shape-ad0yf61u-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
-const sql = neon(dbEnv.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, {
   schema,
