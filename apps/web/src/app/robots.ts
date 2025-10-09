092386ec74be1a27f8020e4e478cd2d672526369
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-10-09 01:07:02 +0100

    disable robots.ts in non proudction deployments

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -1,6 +1,17 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  if (process.env.VERCEL_ENV !== "production") {
+    return {
+      rules: [
+        {
+          userAgent: '*',
+          disallow: ['/',],
+        },
+      ]
+    }
+  }
+
   return {
     rules: [
       {
