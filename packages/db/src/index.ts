3b406892ed8d9c847f8a217b55f6214ca61f954c
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-11-12 02:33:18 +0100

    fix: migrate to planetscale

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,5 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 import { relations } from './schema/relations';
 
-const sql = neon(process.env.DATABASE_URL!);
-
-export const db = drizzle(sql, { schema, relations });
+export const db = drizzle(process.env.DATABASE_URL!, { schema, relations });
