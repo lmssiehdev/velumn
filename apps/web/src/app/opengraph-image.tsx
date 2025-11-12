e8bac4fbfd3ea889e1658e1a06c8183eea5d0b08
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-11-12 23:12:40 +0100

    more metadata fixes

--- apps/web/src/app/opengraph-image.tsx
+++ apps/web/src/app/opengraph-image.tsx
@@ -19,6 +19,7 @@ export default async function Image() {
 			style={{
 				height: "100%",
 				width: "100%",
+				backgroundColor: "#fefcf6",
 				display: "flex",
 				flexDirection: "column",
 				alignItems: "center",
