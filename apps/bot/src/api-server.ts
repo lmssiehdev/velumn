27a13b41612bb80cfa5a9e466f3f4cdfe6d363ab
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2025-12-30 16:16:19 +0100

    fix(CI): fix dashboard build

--- apps/bot/src/api-server.ts
+++ apps/bot/src/api-server.ts
@@ -1,5 +1,4 @@
 import { trpcServer } from "@hono/trpc-server";
-import { zValidator } from "@hono/zod-validator";
 import { apiLogger } from "@repo/logger";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
@@ -7,22 +6,6 @@ import { logger } from "hono/logger";
 import type z from "zod";
 import { getHonoIp } from "./helpers/rate-limit";
 import { botRouter } from "./helpers/trpc";
-
-export function validateParams<Schema extends z.ZodSchema>(
-	schema: Schema,
-	response?: object,
-) {
-	return zValidator("json", schema, (result, c) => {
-		if (!result.success) {
-			return c.json(
-				response ?? {
-					error: "Invalid params",
-				},
-				400,
-			);
-		}
-	});
-}
 
 export const customLogger = (message: string, ...rest: string[]) => {
 	apiLogger.info(`[API] ${message}`, {
